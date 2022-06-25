08646227f3ece9dd3d4681592fc391006d346121
a_belikov <a.belikov@alef.im>
2022-06-25 11:06:33 +0300

    init + db

---
+++ .sql/baseline/V1_0__baseline.sql
@@ -0,0 +1,48 @@
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`
+(
+    `id`    int unsigned NOT NULL AUTO_INCREMENT,
+    `name`  varchar(30),
+    `email` varchar(30),
+    `ip`    int unsigned,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb3;
+
+DROP TABLE IF EXISTS `questions`;
+CREATE TABLE `questions`
+(
+    `id`          int unsigned NOT NULL AUTO_INCREMENT,
+    `title`       varchar(30),
+    `question`    varchar(300),
+    `select_type` enum ('single','multi') NOT NULL DEFAULT 'single' COMMENT 'варианты выбора ответов',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb3;
+
+DROP TABLE IF EXISTS `options`;
+CREATE TABLE `options`
+(
+    `id`          int          NOT NULL AUTO_INCREMENT,
+    `question_id` int unsigned NOT NULL,
+    `option`      varchar(300),
+    PRIMARY KEY (`id`),
+    KEY `question_id` (`question_id`),
+    CONSTRAINT `options_fk1` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb3;
+
+DROP TABLE IF EXISTS `answers`;
+CREATE TABLE `answers`
+(
+    `id`          int          NOT NULL AUTO_INCREMENT,
+    `user_id`     int unsigned NOT NULL,
+    `question_id` int unsigned NOT NULL,
+    `answer`      varchar(300),
+    PRIMARY KEY (`id`),
+    KEY `user_id` (`user_id`),
+    KEY `question_id` (`question_id`),
+    CONSTRAINT `answers_fk1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT,
+    CONSTRAINT `answers_fk2` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb3;
---
+++ .sql/seeders/seeder.sql
@@ -0,0 +1,32 @@
+INSERT users VALUES (1,'Петя', 'petr1@mail.ru', INET_ATON('192.168.99.10'));
+INSERT users VALUES (2,'Вася', 'vasil2@yandex.ru', INET_ATON('192.168.99.11'));
+INSERT users VALUES (3, 'admin', 'admin@tkbbank.ru', INET_ATON('127.0.0.12'));
+
+INSERT questions VALUES (1,'Дни недели', 'Какой ваш любимый день недели?', 'single');
+INSERT questions VALUES (2,'Пальцы', 'Выберите любимые пальцы рук', 'multi');
+INSERT questions VALUES (3,'ПДД', 'Кто первый должен проехать перекрёсток?', 'single');
+INSERT questions VALUES (4, 'Глаза в небе', 'Укажите допустимый цвет глаз у пилота перед вылетом', 'multi');
+
+INSERT options VALUES (1,1, 'понедельник');
+INSERT options VALUES (2,1, 'вторник');
+INSERT options VALUES (3,1, 'среда');
+INSERT options VALUES (4,1, 'четверг');
+INSERT options VALUES (5,1, 'пятница');
+
+INSERT options VALUES (6,2, 'большой');
+INSERT options VALUES (7,2, 'указательный');
+INSERT options VALUES (8,2, 'средний');
+INSERT options VALUES (9,2, 'безымянный');
+INSERT options VALUES (10,2, 'мизинец');
+
+INSERT options VALUES (11,3, 'сначала красивая машинка, остальные потом');
+INSERT options VALUES (12,3, 'сначала красная машинка с белой полосочкой');
+INSERT options VALUES (13,3, 'сначала девочки');
+
+INSERT options VALUES (14,4, 'красный');
+INSERT options VALUES (15,4, 'оранжевый');
+INSERT options VALUES (16,4, 'желтый');
+INSERT options VALUES (17,4, 'зеленый');
+INSERT options VALUES (18,4, 'голубой');
+INSERT options VALUES (19,4, 'синий');
+INSERT options VALUES (20,4, 'фиолетовый в крапинку');
