5f05a58808d94a81afe6b5091941e1d1980d7602
a_belikov <a.belikov@alef.im>
2022-06-27 08:47:22 +0300

    серая подсветка для уже отвеченных вопросов

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
-let radios = document.querySelectorAll('input[type=radio]');
-
+const radios = document.querySelectorAll('input[type=radio]');
 radios && radios.forEach((item) => {
     item.addEventListener("click", (event) => {
         item.parentElement.submit();
     })
 });
 
+const answered = document.querySelectorAll('.answered')
+answered && answered.forEach((item)=>{
+    item.nextSibling.style.color="lightgrey";
+})
